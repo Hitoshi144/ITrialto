f6f62d6716b87b7c1780d7a693a3ad57a96b74f0
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2025-04-12 04:01:14 +0500

    team editing, deleting and create request sending complete

--- backend/src/teams/dto/create-team.dto.ts
+++ backend/src/teams/dto/create-team.dto.ts
@@ -4,6 +4,7 @@ export class CreateTeamDto {
     @IsNotEmpty({message: 'Укажите название команды'})
     title: string
 
+    @IsNotEmpty({message: 'Укажите описание команды'})
     description: string
 
     teamLeaderId: number
