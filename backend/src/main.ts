962285d1358ea9aa48f7a266a5aba09618f15279
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2025-06-08 10:51:03 +0500

    fix app.listen

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   // Настройка Socket.IO
   app.useWebSocketAdapter(new IoAdapter(app));
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 
 bootstrap();
